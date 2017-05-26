81b67013a4a4ddeb037f39ec89c0c452e26fa48f
dinhvanhoang <dinh.van.hoang@framgia.com>
2017-05-26 11:35:56 +0700

    Implements Github Oauth

--- configs/filepaths.go
+++ configs/filepaths.go
@@ -3,4 +3,6 @@ package configs
 const (
 	PATH_GOOGLE_SECRET = "private/google_secret.json"
 	PATH_GOOGLE_OAUTH  = "private/google_oauth.json"
+	PATH_GITHUB_SECRET = "private/github_secret.json"
+	PATH_GITHUB_OAUTH  = "private/github_oauth.json"
 )
--- utilities/server.go
+++ utilities/server.go
@@ -9,14 +9,18 @@ import (
 
 var (
 	googleOauth *GoogleOauth
+	githubOauth *GithubOauth
 	googleCodes = make(map[string]bool)
+	githubCodes = make(map[string]bool)
 )
 
-func Server(google *GoogleOauth) {
+func Server(google *GoogleOauth, github *GithubOauth) {
 	var addr = fmt.Sprintf(":%v", configs.SERVER_PORT)
 	googleOauth = google
+	githubOauth = github
 	r := mux.NewRouter()
 	r.HandleFunc(configs.SERVER_GOOGLE_CALLBACK, googleRoute).Methods("GET")
+	r.HandleFunc(configs.SERVER_GITHUB_CALLBACK, githubRoute).Methods("GET")
 	http.ListenAndServe(addr, r)
 }
 
@@ -35,4 +39,21 @@ func googleRoute(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusBadRequest)
 	w.Write([]byte("[Google Oauth] Unable to read Google authorization code."))
+}
+
+func githubRoute(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	code, ok := params["code"]
+	if ok {
+		code := code[0]
+		if _, ok := githubCodes[code]; !ok {
+			githubOauth.codeChan <- code
+			githubCodes[code] = true
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(code))
+		return
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte("[Github Oauth] Unable to read Google authorization code."))
 }
